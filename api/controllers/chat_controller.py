057764d56e013abc90257c821988f135472ddf6b
Christo <christo.pb@caddayn.com>
2025-12-09 10:01:26 -0500

    fixed error responmse

--- api/controllers/chat_controller.py
+++ api/controllers/chat_controller.py
@@ -99,7 +99,7 @@ def send_message():
         error_message = "This question is outside the scope of this medical training tool. Please provide a clinical question to proceed."
 
         db.add_message(chat_id=chat_id, message=error_message, role='bot')
-        return jsonify({"message": error_message}), 404
+        return jsonify({"response": error_message, "role":"bot"}), 404
 
     old_message = []
     # add conversation history
